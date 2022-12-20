d0e08b381fe688db65e7b8089e49e4aedaa15197
Ashutosh rai <mukutrai12@gmail.com>
2022-12-20 08:17:33 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import react from "react";
+import { useState } from "react/cjs/react.production.min";
 import "./styles.css";
+import React from "react";
+import Counter from "./Counter";
 
 export default function App() {
+  const [counter, setCounter] = React.useState(0);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>I am parent component and my count is: {counter}</h1>
+      <Counter currentCount={counter} setCounter={setCounter} />
     </div>
   );
 }
